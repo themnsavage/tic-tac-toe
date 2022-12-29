4b9f9f62701b9344c99ec0571d9fcc7f6fe61490
thawki <Reedhawkins2000@gmail.com>
2022-12-29 02:07:45 -0600

    feat(game); adding getter functions and unit tests

--- game.py
+++ game.py
@@ -1,8 +1,19 @@
 class Game:
-    def __init__(self, player1_name, player2_name): 
+    def __init__(self, player1_name = "player 1", player2_name = "player 2"): 
         self._game_board = [['','',''],
                             ['','',''],
                             ['','','']
                             ]        
         self._player1_name = player1_name
         self._player2_name = player2_name
+
+
+    def get_game_board(self):
+        return self._game_board
+
+    
+    def get_player1_name(self):
+        return self._player1_name
+
+    def get_player2_name(self):
+        return self._player2_name
--- test_game.py
+++ test_game.py
@@ -15,4 +15,30 @@ def test_game_init():
     assert game_object._player1_name ==  player1_name
     assert game_object._player2_name == player2_name
     assert game_object._game_board == default_game_board
+
+
+def test_get_game_board():
+    default_game_board = [
+                        ['','',''],
+                        ['','',''],
+                        ['','','']
+                        ]
+
+    game_object = Game()
+
+    assert default_game_board == game_object.get_game_board()
+
+
+def test_get_player_names():
+    player1_name = 'Noah'
+    player2_name = 'Reed'
+
+    game_object = Game(player1_name, player2_name)
+
+    assert player1_name == game_object.get_player1_name()
+    assert player2_name == game_object.get_player2_name()
+
+
+
+
     
