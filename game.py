fc7108a89560974d1657e22975d9184265f42dca
themnsavage <mnsavage@crimson.ua.edu>
2022-12-29 18:27:21 -0600

    refactor(game): get rid of player 1 and 2 name param in init

--- game.py
+++ game.py
@@ -1,22 +1,12 @@
 class Game:
-    def __init__(self, player1_name = "player 1", player2_name = "player 2"): 
+    def __init__(self): 
         self._game_board = [['','',''],
                             ['','',''],
                             ['','','']
                             ]        
-        self._player1_name = player1_name
-        self._player2_name = player2_name
-
 
     def get_game_board(self):
         return self._game_board
-
-    
-    def get_player1_name(self):
-        return self._player1_name
-
-    def get_player2_name(self):
-        return self._player2_name
 
     def add_turn_to_game_board(self, grid_number, game_piece):
         grid = {
--- test_game.py
+++ test_game.py
@@ -2,18 +2,14 @@ import pytest
 from game import Game
 
 def test_game_init():
-    player1_name = 'Noah'
-    player2_name = 'Reed'
     default_game_board = [
                         ['','',''],
                         ['','',''],
                         ['','','']
                         ]        
 
-    game_object = Game(player1_name, player2_name)
-    
-    assert game_object._player1_name ==  player1_name
-    assert game_object._player2_name == player2_name
+    game_object = Game()
+
     assert game_object._game_board == default_game_board
 
 
@@ -27,17 +23,6 @@ def test_get_game_board():
     game_object = Game()
 
     assert default_game_board == game_object.get_game_board()
-
-
-def test_get_player_names():
-    player1_name = 'Noah'
-    player2_name = 'Reed'
-
-    game_object = Game(player1_name, player2_name)
-
-    assert player1_name == game_object.get_player1_name()
-    assert player2_name == game_object.get_player2_name()
-
 
 def test_add_turn_to_game_board():
     grid_number = 4
