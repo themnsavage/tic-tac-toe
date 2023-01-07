09065b05ae1be57097cc3e753fdf5fcc210064d5
themnsavage <mnsavage@crimson.ua.edu>
2023-01-07 03:50:07 -0600

    feat(tic tac toe): add ai game mode

--- module/app/tic_tac_toe.py
+++ module/app/tic_tac_toe.py
@@ -1,5 +1,5 @@
 from module.app.game_board import Game_Board
-
+from module.app.ai import AI
 class Tic_Tac_Toe:
     def __init__(self):
         self._player_dict = {
@@ -9,6 +9,35 @@ class Tic_Tac_Toe:
         self._game_board_object = Game_Board()
     
     def start(self):
+        game_mode = input("Enter 1 for player vs player \nEnter 2 for player vs ai\n")
+        if game_mode == '1':
+            self._player_vs_player()
+        else:
+            self._player_vs_ai()
+
+    def _player_vs_ai(self):
+        keeping_playing = True
+        ai_player = AI(self._game_board_object, self._player_dict["player 2"])
+
+        while(keeping_playing):
+            for player, game_piece in self._player_dict.items():
+                if player == "player 1":
+                    self._print_current_game_board()
+                current_player_turn = self._get_player_turn(player) if player == "player 1" else ai_player.get_best_move()
+                self._game_board_object.add_turn_to_game_board(current_player_turn, game_piece)
+                
+                if self._game_board_object.check_for_winner():
+                    print(f"{player} has won")
+                    self._game_board_object.print_game_board()
+                    keeping_playing = False
+                    break
+                elif self._game_board_object.is_game_board_filled():
+                    print('board is filled tie')
+                    self._game_board_object.print_game_board()
+                    keeping_playing = False
+                    break
+
+    def _player_vs_player(self):
         keeping_playing = True
         while(keeping_playing):
             for player, game_piece in self._player_dict.items():
@@ -26,7 +55,7 @@ class Tic_Tac_Toe:
                     self._game_board_object.print_game_board()
                     keeping_playing = False
                     break
-    
+
     def _print_current_game_board(self):
         print('Current Game Board:')
         self._game_board_object.print_game_board()
